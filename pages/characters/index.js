00d9a4d95bef2ee6e40b902139cdf4482ede9d84
hoangnm-ndm <hoangnm.ndm@gmail.com>
2022-05-06 17:41:34 +0700

    update home page

---
+++ pages/characters/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function index() {
+  return (
+    <>
+    <h1>Characters Home pages</h1>
+    </>
+  )
+}
+
+export default index
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,59 @@
 import styled from 'styled-components'
+import Head from 'next/head'
+import Character from '../components/Character.js';
+import { useState } from 'react/cjs/react.development';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+export default function Characters({ characters }) {
+  const [value, setValue] = useState(12)
 
-export default function Home() {
-  return <Title>My page</Title>
+  const handleShowMore = () => {
+    setValue(value + 12)
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Star Wars</title>
+      </Head>
+      <HomeScreenContainer>
+        <StarFieldLeft />
+        <CharactersContainer>
+          {characters
+          .filter((character) => character.id !== 28 && character.id !==77 )
+          .map(
+            (character, index) => 
+              index < value && (
+                <Character character = {character} key = {character.id} />
+              )
+          )
+          }
+        </CharactersContainer>
+        <StarFieldRight />
+      </HomeScreenContainer>
+    </>
+  )
 }
+
+export async function getStaticProps(context) {
+  const characters = await fetch("https://akabab.github.io/starwars-api/api/all.json")
+  .then((res) => res.json())
+  
+  return {
+    props: {
+      characters,
+    },
+  }
+}
+
+const HomeScreenContainer = styled.div`
+
+`
+const StarFieldLeft = styled.div`
+
+`
+const CharactersContainer = styled.div`
+
+`
+const StarFieldRight = styled.div`
+
+`
